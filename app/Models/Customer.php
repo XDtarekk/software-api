e95f9196badb12b95daaa5b6d3492195761635d0
XDtarekk <krazyrt@gmail.com>
2022-11-10 21:10:14 +0200

    authController added and register,login and loggout functions were made

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -1,13 +1,13 @@
 <?php
 
 namespace App\Models;
-
+use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Customer extends Model
 {
-    use HasFactory;
+    use HasFactory, HasApiTokens;
     protected $table = 'customers';
     protected $fillable=[
         'name',
